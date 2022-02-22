41998b98a7b3c3a810128b1ee48d8145298e0ad8
Prati-1234 <pratikshapalkar141@gmail.com>
2022-02-22 13:34:21 +0530

    change render json in regsitration controller and added render sucess method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,18 +4,12 @@ class RegistrationsController < DeviseTokenAuth::RegistrationsController
   def create
     user = User.new(sign_up_params)
     if user.save 
-      render json: user.as_json, status: :ok, message:'User created successfully'
+      render_success 200, true, 'User created successfully',user.as_json
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      return_error 500, false, errors, {}
     end
   end
-
-  def show
-    render json: User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: {}, status: :not_found
-  end
-
+  
   private
 
   # parameters for sign up
